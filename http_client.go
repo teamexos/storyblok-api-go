cc1e1daffc3c2268ea50844a00467dda2c7a1245
Michael Clifford <cliffom@gmail.com>
2020-11-10 15:58:19 -0800

    Update HTTP Client defaults

--- http_client.go
+++ http_client.go
@@ -1,8 +1,18 @@
 package storyblok
 
 import (
+	"net"
 	"net/http"
 	"time"
+)
+
+// HTTP client defaults
+var (
+	defaultHTTPTransportDisableCompression = false
+	defaultHTTPTransportIdleConnTimeout    = 30 * time.Second
+	defaultHTTPTransportMaxIdleConns       = 5
+	defaultHTTPTransportDialContextTimeout = 5 * time.Second
+	defaultHTTPClientTimeout               = time.Minute
 )
 
 // HTTPClient interface
@@ -12,7 +22,21 @@ type HTTPClient interface {
 
 // DefaultHTTPClient returns the default REST client
 func DefaultHTTPClient() HTTPClient {
+
+	// Instantiate gzip client with a 5 second timeout on waiting for the
+	// remote server to accept the connection and a 30 second timeout
+	// for no activity over the connection
+	transport := &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout: defaultHTTPTransportDialContextTimeout,
+		}).DialContext,
+		MaxIdleConns:       defaultHTTPTransportMaxIdleConns,
+		IdleConnTimeout:    defaultHTTPTransportIdleConnTimeout,
+		DisableCompression: defaultHTTPTransportDisableCompression,
+	}
+
 	return &http.Client{
-		Timeout: time.Minute,
+		Timeout:   defaultHTTPClientTimeout,
+		Transport: transport,
 	}
 }
