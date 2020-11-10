5bbaa1abb7092204087a2b5b1b8bcc8e3efcd554
Michael Clifford <cliffom@gmail.com>
2020-11-10 12:59:05 -0800

    cleanups

---
+++ story_input.go
@@ -0,0 +1,29 @@
+package storyblok
+
+import (
+	"net/url"
+
+	"github.com/google/go-querystring/query"
+)
+
+// StoryInput defines the valid input parameters for GetStory
+type StoryInput struct {
+	FindBy           string `url:"find_by,omitempty"`
+	Version          string `url:"version,omitempty"`
+	ResolveLinks     string `url:"find_by,omitempty"`
+	ResolveRelations string `url:"resolve_relations,omitempty"`
+	FromRelease      int    `url:"from_release,omitempty"`
+	CV               string `url:"cv,omitempty"`
+	Language         string `url:"language,omitempty"`
+	FallbackLang     string `url:"fallback_lang,omitempty"`
+}
+
+// QueryParams returns a GetStoryInput struct as a query param string
+func (i StoryInput) QueryParams() (url.Values, error) {
+	v, err := query.Values(i)
+	if err != nil {
+		return nil, err
+	}
+
+	return v, nil
+}
