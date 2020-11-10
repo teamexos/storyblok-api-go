b1367847229d0367d6800fc843c2163a3bf1923a
Michael Clifford <cliffom@gmail.com>
2020-11-10 06:33:31 -0800

    Use map string interface for content

--- story.go
+++ story.go
@@ -10,24 +10,24 @@ type (
 
 	// Story is a struct representation of the main story object
 	Story struct {
-		ID               int               `json:"id"`
-		UUID             string            `json:"uuid"`
-		Name             string            `json:"name"`
-		Slug             string            `json:"slug"`
-		FullSlug         string            `json:"full_slug"`
-		DefaultFullSlug  string            `json:"default_full_slug"`
-		CreatedAt        string            `json:"created_at"`
-		PublishedAt      string            `json:"published_at"`
-		FirstPublishedAt string            `json:"first_published_at"`
-		ReleaseID        string            `json:"release_id"`
-		Lang             string            `json:"lang"`
-		Content          *Content          `json:"content"`
-		Position         int               `json:"position"`
-		IsStartPage      bool              `json:"is_startpage"`
-		ParentID         int               `json:"parent_id"`
-		GroupID          string            `json:"group_id"`
-		Alternates       []*Alternate      `json:"alternates"`
-		TranslatedSlugs  []*TranslatedSlug `json:"translated_slugs"`
+		ID               int                    `json:"id"`
+		UUID             string                 `json:"uuid"`
+		Name             string                 `json:"name"`
+		Slug             string                 `json:"slug"`
+		FullSlug         string                 `json:"full_slug"`
+		DefaultFullSlug  string                 `json:"default_full_slug"`
+		CreatedAt        string                 `json:"created_at"`
+		PublishedAt      string                 `json:"published_at"`
+		FirstPublishedAt string                 `json:"first_published_at"`
+		ReleaseID        string                 `json:"release_id"`
+		Lang             string                 `json:"lang"`
+		Content          map[string]interface{} `json:"content"`
+		Position         int                    `json:"position"`
+		IsStartPage      bool                   `json:"is_startpage"`
+		ParentID         int                    `json:"parent_id"`
+		GroupID          string                 `json:"group_id"`
+		Alternates       []*Alternate           `json:"alternates"`
+		TranslatedSlugs  []*TranslatedSlug      `json:"translated_slugs"`
 	}
 
 	// StoryResponse represents the structured reponse from Storyblok
