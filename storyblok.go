94e945647d4ba86665fde935e81164fc890f5835
Karolina Hidalgo <khidalgoleon@teamexos.com>
2020-11-16 16:20:18 -0700

    use latest space version for all story requests

--- storyblok.go
+++ storyblok.go
@@ -12,19 +12,49 @@ const baseURLv1 = "https://api.storyblok.com/v1/cdn"
 
 // Client is the client struct used to access the Storyblok APIs
 type Client struct {
-	baseURL    string
-	token      string
-	HTTPClient HTTPClient
+	baseURL      string
+	token        string
+	SpaceVersion int
+	HTTPClient   HTTPClient
 }
 
 // NewClient returns a pointer to Client
 func NewClient(token string) *Client {
-	return &Client{
-		baseURL:    baseURLv1,
-		token:      token,
-		HTTPClient: DefaultHTTPClient(),
+	// get the latest cache version
+	client := &Client{
+		baseURL:      baseURLv1,
+		token:        token,
+		HTTPClient:   DefaultHTTPClient(),
 	}
+	// Get the latest space version for all subsequent story calls
+    space, err := client.GetLatestSpace(context.Background())
+    if err == nil {
+		client.SpaceVersion = space.Version
+	}
+	return client
 }
+
+// GetLatestSpace gets the latest space version so that we can always deliver the latest content
+//
+// https://www.storyblok.com/docs/api/content-delivery#topics/cache-invalidation
+func (c *Client) GetLatestSpace(ctx context.Context) (*Space, *ResponseError) {
+	endpoint := fmt.Sprintf("%s/spaces/me", c.baseURL)
+	req, err := http.NewRequestWithContext(ctx, "GET", endpoint, nil)
+	if err != nil {
+		return nil, NewResponseError(http.StatusInternalServerError, errCodeRequestSetupFailed)
+	}
+
+	q := req.URL.Query()
+	q.Add("token", c.token)
+	req.URL.RawQuery = q.Encode()
+
+	res := SpaceResponse{}
+	if err := c.sendRequest(req, &res); err != nil {
+		return nil, err
+	}
+	return &res.Space, nil
+}
+
 
 // GetStory returns a story object for the full_slug, id or uuid if
 // authenticated using a preview or public token.
@@ -50,6 +80,9 @@ func (c *Client) GetStory(ctx context.Context,
 
 	q := req.URL.Query()
 	q.Add("token", c.token)
+	if c.SpaceVersion != 0 {
+		q.Add("cv", string(c.SpaceVersion))
+	}
 	req.URL.RawQuery = q.Encode()
 
 	res := StoryResponse{}
@@ -79,7 +112,8 @@ func (c *Client) sendRequest(req *http.Request, v interface{}) *ResponseError {
 	}
 
 	if err = json.NewDecoder(res.Body).Decode(&v); err != nil {
-		return NewResponseError(http.StatusInternalServerError, errCodeRequestDecodeFailed)
+		return NewResponseError(http.StatusInternalServerError,
+			fmt.Sprintf("%s: %s", errCodeRequestDecodeFailed, err.Error()))
 	}
 
 	return nil
